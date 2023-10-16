70cd7c2fa8b0e48ad4724962f786f6f7e450886d
layanmoyura <layanmoyura@gmail.com>
2023-10-16 12:21:10 +0530

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Soft_Gallery_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
