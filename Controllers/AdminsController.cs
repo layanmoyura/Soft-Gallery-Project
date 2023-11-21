388a8371c7641c79f6408a5cd490bdcab3129f1d
layanmoyura <layanmoyura@gmail.com>
2023-11-21 16:43:08 +0530

    database and view setup for jwt

---
+++ Controllers/AdminsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PresentationLayer.Controllers
+{
+    public class AdminsController : Controller
+    {
+        public ViewResult SignUp()
+        {
+            return View();
+        }
+
+
+        public ViewResult LogIn()
+        {
+            return View();
+        }
+    }
+}
---
+++ Model/AdminModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Xamarin.Essentials;
+
+namespace PresentationLayer.Models
+{
+    public class AdminModel
+    {
+        public int UserID { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        [Required]
+        public string EmailAddress { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+
+        [Required]
+        public string LastName { get; set; }
+
+        [DataType(DataType.Password)]
+        [Required]
+        public string Password { get; set; }
+    }
+}
